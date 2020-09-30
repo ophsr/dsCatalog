94c78f338d2169346964cee050d59e7c00a005b1
Pedro <phsr2001@gmail.com>
2020-09-30 19:15:17 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dsCatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dsCatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.devsuperior.dsCatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dsCatalog.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResourceExceptionHandler{
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardErro> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardErro> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
         StandardErro error = new StandardErro();
         error.setTimestamp(Instant.now());
         error.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/java/com/devsuperior/dsCatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dsCatalog/services/CategoryService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.devsuperior.dsCatalog.dto.CategoryDTO;
 import com.devsuperior.dsCatalog.entities.Category;
 import com.devsuperior.dsCatalog.repositories.CategoryRepository;
-import com.devsuperior.dsCatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dsCatalog.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,35 +19,46 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CategoryService {
 
-    //injeção de dependencia(repository)
+    // injeção de dependencia(repository)
     @Autowired
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
+    public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
 
         return list.stream().map(entity -> new CategoryDTO(entity)).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public CategoryDTO findById(long id){
+    public CategoryDTO findById(long id) {
         Optional<Category> obj = repository.findById(id);
 
-        Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 
         return new CategoryDTO(entity);
     }
 
     @Transactional
-	public CategoryDTO insert(CategoryDTO dto) {
+    public CategoryDTO insert(CategoryDTO dto) {
 
         Category entity = new Category();
         entity.setName(dto.getName());
-        entity=repository.save(entity);
-		return new CategoryDTO(entity);
-	}
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 
+    @Transactional
+    public CategoryDTO update(long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+    }
 
 
 }
