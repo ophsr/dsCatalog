3cb30cd5cadec881de9635c9636e0b32f48a0b9b
Pedro <phsr2001@gmail.com>
2020-10-25 13:09:54 -0300

    ORM mapping, database seeding(user,role)

--- backend/src/main/java/com/devsuperior/dsCatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dsCatalog/entities/Category.java
@@ -2,15 +2,20 @@ package com.devsuperior.dsCatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
 
 //Indentifica que é uma entidate e dar nome a tabela
 @Entity
@@ -30,6 +35,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy="categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -72,6 +80,10 @@ public class Category implements Serializable {
     @PreUpdate
     public void preUpdate() {
         updatedAt = Instant.now();
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
