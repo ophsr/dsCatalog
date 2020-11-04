dc16f473a81a3fa7bff7c8b383cd92a7197744d7
Pedro <phsr2001@gmail.com>
2020-11-03 23:58:33 -0300

    Basic Make login

--- front-web/src/pages/Auth/Components/Login/index.tsx
+++ front-web/src/pages/Auth/Components/Login/index.tsx
@@ -5,9 +5,10 @@ import AuthCard from '../Card';
 import { useForm } from 'react-hook-form';
 
 import './styles.scss';
+import { makeLogin } from 'core/utils/request';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
@@ -15,8 +16,8 @@ const Login = () => {
     const { register, handleSubmit } = useForm<FormData>(); // initialize the hook
 
     const onSubmit = (data: FormData) => {
-        console.log("teste")
         console.log(data);
+        makeLogin(data)
     };
 
     return (
@@ -27,7 +28,7 @@ const Login = () => {
                     className="form-control input-base margin-botton-30"
                     placeholder="Email"
                     ref={register}
-                    name="email"
+                    name="username"
                 />
 
                 <input
